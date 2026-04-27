e210b5301ecfe31209e7b430b3b74a87731318fd
p0rnst4r77170-design <p0rnst4r77170@gmail.com>
2026-04-27 16:10:56 +0200

    Update index.js

--- api/index.js
+++ api/index.js
@@ -5,15 +5,18 @@ module.exports = async (req, res) => {
   if (!q) return res.send("Ecris un texte !");
 
   try {
+    // On fait la traduction
     const result = await translate(q, { 
       to: 'th',
       forceBatch: false 
     });
+
+    // On cherche la phonétique dans les données renvoyées par Google
+    // Si 'transliteration' existe, on l'affiche, sinon on donne le texte thaï
+    const reponse = result.transliteration || result.text;
     
-    // Si Google renvoie la phonétique (ex: Sawatdee), on l'affiche. 
-    // Sinon on affiche le texte.
-    res.send(result.transliteration || result.text);
+    res.send(reponse);
   } catch (err) {
-    res.send("Erreur de traduction");
+    res.send("Erreur");
   }
 };
